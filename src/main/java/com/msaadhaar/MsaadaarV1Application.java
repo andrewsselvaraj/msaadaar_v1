646e9dde7f8f41dc4356073681c7e9b1e8ac7e49
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-06 01:28:25 +0530

    Base

---
+++ src/main/java/com/msaadhaar/MsaadaarV1Application.java
@@ -0,0 +1,13 @@
+package com.msaadhaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaadaarV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaadaarV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/msaadhaar/MsaadaarV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msaadhaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaadaarV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
