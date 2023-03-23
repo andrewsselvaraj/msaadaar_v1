d72d58b5016b42afc3828bd0bfc477edd5e66475
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-23 17:53:24 +0530

    AADHAAR VALIDATION

--- src/main/java/com/msaadhaar/controller/AadaarController.java
+++ src/main/java/com/msaadhaar/controller/AadaarController.java
@@ -19,4 +19,16 @@ public class AadaarController {
 			hm.put("status", "fail");
 		return hm;
 	}
+	
+	@RequestMapping(value ="/validateAadaarGet", method = RequestMethod.GET)
+	public HashMap<String,String> validateAadaarGet(String aadaarNo)
+	{
+		HashMap<String,String> hm = new  HashMap<String,String>();
+		
+		if(Integer.parseInt(aadaarNo)%2==0)
+			hm.put("status", "success");
+		else
+			hm.put("status", "fail");
+		return hm;
+	}
 }
